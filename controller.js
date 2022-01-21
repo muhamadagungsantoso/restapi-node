7289f94492adb50b3197dc9180d9c78ee81d0e6a
Muhamad Agung Santoso <muhamadagung70674@gmail.com>
2022-01-21 19:41:54 +0700

    get barang

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ const connection = require("./koneksi");
 exports.index = function (req, res) {
     response.ok("Rest server berjalan!", res);
 };
+
+// menampilkan semua data
+exports.getBarang = function(req, res) {
+    connection.query("SELECT * FROM tb_barang", function(error, rows, field) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
